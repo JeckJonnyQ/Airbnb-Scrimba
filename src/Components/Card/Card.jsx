4d4d1ff4aefe02952f6a14f7f62ddd452c148ec1
Reut Vladislav <reut-2011@mail.ru>
2024-10-22 14:02:13 +0300

    rafactor: change path to images

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import "./Card.scss";
+import StarIcon from "../../assets/Star.png";
 
 export default function Card(props) {
   let badgeText;
@@ -11,15 +12,11 @@ export default function Card(props) {
   return (
     <div className="card">
       {badgeText && <div className="card__badge">{badgeText}</div>}
-      <img src={`./src/assets/${props.coverImg}`} className="card__image" />
+      <img src={props.coverImg} className="card__image" />
 
       <div className="card__content">
         <div className="card__stats">
-          <img
-            src="./src/assets/Star.png"
-            className="card__stats_star"
-            alt="Star Icon"
-          />
+          <img src={StarIcon} className="card__stats_star" alt="Star Icon" />
           <span className="card__stats_rating">{props.stats.rating}</span>
           <span className="card__stats_count gray">
             ({props.stats.reviewCount}) •
